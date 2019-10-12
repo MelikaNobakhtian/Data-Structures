b951e020c369bf1aee67ba185cde3861f06a3b4d
MelikaNobakhtian <haminob48@gmail.com>
2019-10-12 17:32:59 +0330

    A3

---
+++ A3/A3/Q5LCM.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using TestCommon;
+
+namespace A3
+{
+    public class Q5LCM : Processor
+    {
+        public Q5LCM(string testDataName) : base(testDataName) { }
+
+        public override string Process(string inStr) =>
+            TestTools.Process(inStr, (Func<long, long, long>)Solve);
+
+        public long Solve(long a, long b)
+        {
+            var gcd = GCD(a, b);
+            return (a / gcd) * b;
+        }
+
+        public long GCD(long a, long b)
+        {
+            if (a < b)
+                (b, a) = (a, b);
+            if (b == 0)
+                return a;
+            return GCD(b, a % b);
+        }
+    }
+}
