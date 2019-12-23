075360d3c07451b5a0396b3dc020d91c64defa49
MelikaNobakhtian <haminob48@gmail.com>
2019-12-23 14:47:47 +0330

    2

---
+++ E2b/E2b/Q1ImplementNextForBST.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TestCommon;
+
+namespace E2b
+{
+    public class Q1ImplementNextForBST : Processor
+    {
+        public Q1ImplementNextForBST(string testDataName) : base(testDataName) 
+        {
+            //this.ExcludeTestCaseRangeInclusive(1, 10);
+        }
+        public override string Process(string inStr)
+        {
+            long n, node;
+            var lines = inStr.Split(TestTools.NewLineChars, StringSplitOptions.RemoveEmptyEntries);
+            TestTools.ParseTwoNumbers(lines[0], out n, out node);
+            var bst = lines[1].Split(TestTools.IgnoreChars, StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => long.Parse(x))
+                .ToArray();
+
+            return Solve(n, node, bst).ToString();
+        }
+
+        public long Solve(long n, long node, long[] BST)
+        {
+            return Next(node, n, BST);           
+        }
+
+        public long RightChild(long idx)
+        {
+            return 2 * idx + 2;
+        }
+        public long LeftChild(long idx)
+        {
+            return 2 * idx + 1;
+        }
+        public long Next(long node,long n,long[] tree)
+        {
+            var right = RightChild(node);
+            if (right < n && tree[right] != -1)
+            {
+                return LeftDes(right, n, tree);
+            }
+            else
+                return RightAnc(node, n, tree);
+        }
+
+        private long RightAnc(long node, long n, long[] tree)
+        {
+            if (node == 0)
+                return -1;
+            var parent = Parent(node);
+            if (tree[parent] > tree[node])
+                return parent;
+            else
+                return RightAnc(parent, n, tree);
+           
+        }
+
+        public long Parent(long idx)
+        {
+            return (idx - 1) / 2;
+        }
+
+        private long LeftDes(long node, long n, long[] tree)
+        {
+
+            var left = LeftChild(node);
+
+            if (left < n && tree[left] == -1)
+                return node;
+            else if (left < n)
+                return LeftDes(left, n, tree);
+            else
+                return -1;
+        }
+    }
+}
---
+++ E2b/E2b/Q2HashTableAttack.cs
@@ -0,0 +1,146 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using TestCommon;
+
+namespace E2b
+{
+    public class Q2HashTableAttack : Processor
+    {
+        public Q2HashTableAttack (string testDataName) : base(testDataName) 
+        {
+        }
+
+        public override string Process(string inStr)
+        {
+            long bucketCount = long.Parse(inStr);
+            return string.Join("\n", Solve(bucketCount));
+        }
+
+        public string[] Solve(long bucketCount)
+        {
+            long size = 9 * bucketCount / 10;
+            List<string> result = new List<string>();
+            Random rnd = new Random();
+            var idx = rnd.Next(0, 25);
+            var str = CapChars[idx].ToString();
+            var hash = GetBucketNumber(str, bucketCount);
+            for (int i = 0; i < 26 && result.Count < size; i++)
+            {
+                string str1 = CapChars[i].ToString();
+                if (hash == GetBucketNumber(str1, bucketCount))
+                    result.Add(str1);
+            }
+            for (int i = 0; i < 26 && result.Count < size; i++)
+            {
+                string str1 = CapChars[i].ToString();
+                if (hash == GetBucketNumber(str1, bucketCount))
+                    result.Add(str1);
+            }
+            for (int i = 0; i < 10 && result.Count < size; i++)
+            {
+                string str1 = Numbers[i].ToString();
+                if (hash == GetBucketNumber(str1, bucketCount))
+                    result.Add(str1);
+            }
+            if (result.Count == size)
+                return result.ToArray();
+
+            for(int i = 0; i < 26 && result.Count<size; i++)
+            {
+                for(int j = 0; j < 26 && result.Count<size; j++)
+                {
+                    string test = CapChars[i].ToString() + CapChars[j].ToString();
+                    if (hash == GetBucketNumber(test, bucketCount))
+                        result.Add(test);
+                }
+            }
+            for (int i = 0; i < 26 && result.Count < size; i++)
+            {
+                for (int j = 0; j < 26 && result.Count < size; j++)
+                {
+                    string test = LowChars[i].ToString() + LowChars[j].ToString();
+                    if (hash == GetBucketNumber(test, bucketCount))
+                        result.Add(test);
+                }
+            }
+
+            for (int i = 0; i < 10 && result.Count < size; i++)
+            {
+                for (int j = 0; j < 10 && result.Count < size; j++)
+                {
+                    string test = Numbers[i].ToString() + Numbers[j].ToString();
+                    if (hash == GetBucketNumber(test, bucketCount))
+                        result.Add(test);
+                }
+            }
+
+            while (result.Count < size)
+            {
+                string a=new string(Enumerable.Repeat(CapChars, 3).Select(s => s[rnd.Next(s.Length)]).ToArray());
+                if (hash == GetBucketNumber(a, bucketCount) && !result.Contains(a))
+                    result.Add(a);
+                if (result.Count < size)
+                {
+                    string b= new string(Enumerable.Repeat(LowChars, 3).Select(s => s[rnd.Next(s.Length)]).ToArray());
+                    if (hash == GetBucketNumber(b, bucketCount) && !result.Contains(b))
+                        result.Add(b);
+                }
+
+                if (result.Count < size)
+                {
+                    string c = new string(Enumerable.Repeat(Numbers, 3).Select(s => s[rnd.Next(s.Length)]).ToArray());
+                    if (hash == GetBucketNumber(c, bucketCount) && !result.Contains(c))
+                        result.Add(c);
+                }
+
+            }
+
+            return result.ToArray();
+            //for (int i = 0; i < 25 && result.Count < size; i++)
+            //{
+            //    string str1 =$"{CapChars[i]}{CapChars[i+1]}";
+            //    if (hash == GetBucketNumber(str1, bucketCount))
+            //        result.Add(str1);
+            //}
+            //for (int i = 0; i < 25 && result.Count < size; i++)
+            //{
+            //    string str1 = $"{LowChars[i]}{LowChars[i + 1]}";
+            //    if (hash == GetBucketNumber(str1, bucketCount))
+            //        result.Add(str1);
+            //}
+
+            //return result.ToArray();
+
+
+        }
+
+        #region Chars
+        static char[] LowChars = Enumerable
+            .Range(0, 26)
+            .Select(n => (char)('a' + n))
+            .ToArray();
+
+        static char[] CapChars = Enumerable
+            .Range(0, 26)
+            .Select(n => (char)('A' + n))
+            .ToArray();
+
+        static char[] Numbers = Enumerable
+            .Range(0, 10)
+            .Select(n => (char)('0' + n))
+            .ToArray();
+
+        static char[] AllChars = 
+            LowChars.Concat(CapChars).Concat(Numbers).ToArray();
+        #endregion
+
+
+        // پیاده‌سازی مورد استفاده دات‌نت برای پیدا کردن شماره باکت
+        // https://referencesource.microsoft.com/#mscorlib/system/collections/generic/dictionary.cs,bcd13bb775d408f1
+        public static long GetBucketNumber(string str, long bucketCount) =>
+            (str.GetHashCode() & 0x7FFFFFFF) % bucketCount;
+    }
+}
